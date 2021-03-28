4f809a0b6df00ee33846d833cbef0b6dcbf8ee89
krishnakanththammana <krishnakanththammana@gmail.com>
2021-03-28 15:37:56 +0000

    Stock

---
+++ index.js
@@ -0,0 +1,24 @@
+const program = require('commander');
+const axios = require('axios');
+const pckg = require('./package.json')
+const constants = require('./constants.json')
+
+program.version(pckg.version)
+
+program
+  .arguments('<stock>')
+  .description('get stock value')
+  .action((stock) => {
+    console.log("fetching data...");
+    axios.get(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${stock}&apikey=${constants.key}`)
+        .then(function (response) {
+            // handle success
+            console.log(`Stock value: ${response.data['Global Quote']['05. price']}`);
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
+  })
+
+  program.parse(process.argv);
