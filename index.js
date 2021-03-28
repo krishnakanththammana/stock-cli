3d1ed3be9c49694b5229280c69d05f76c49ba329
krishnakanththammana <krishnakanththammana@gmail.com>
2021-03-28 16:23:50 +0000

    update index

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-import { Command } from 'commander';
+#! /usr/bin/env node
+const Command = require('commander').Command;
 const program = new Command();
-import { get } from 'axios';
-import { version } from './package.json';
-import { key } from './constants.json';
+const axios = require('axios');
+const pckg = require('./package.json')
+const constants = require('./constants.json')
 
-program.version(version)
+program.version(pckg.version)
 
 program
   .arguments('<stock>')
   .description('get stock value')
   .action((stock) => {
     console.log("fetching data...");
-    get(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${stock}&apikey=${key}`)
+    axios.get(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${stock}&apikey=${constants.key}`)
         .then(function (response) {
             // handle success
             console.log(`Stock value: ${response.data['Global Quote']['05. price']}`);
